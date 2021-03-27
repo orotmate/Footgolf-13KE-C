23b35053f3df4002d40298e7185b381671ef016d
orotmate <orotmate@gmail.com>
2021-03-27 18:02:39 +0100

    **Megoldás osztály** Fixes #1

--- src/Megoldás.ts
+++ src/Megoldás.ts
@@ -3,4 +3,25 @@ import fs from "fs";
 
 export default class Megoldás{
     private _versenyzők: Versenyző[] = [];
+
+    public get versenyzőkSzáma(): number{
+        return this._versenyzők.length;
+    }
+    public get nőiVersenyzőkSzáma(): number{
+        return this._versenyzők[1].kategória;
+    }
+    public get összPont(): number{
+        return this.
+    }
+    public get nőiBajnok(): string{
+        return this.
+    }
+    constructor(forrás: string){
+        const sorok: string[] = fs.readFileSync(forrás).toString().split("\n");
+        for (let i = 0; i < sorok.length; i++) {
+            if (sorok[i].length != 0){
+                this._versenyzők.push(new Versenyző(sorok[i]));
+            }
+        }
+    }
 }
