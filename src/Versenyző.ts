7a0091348a74785cb77804c1b09e7915e28d7a1d
gyurcsikdavid <davd121111@outlook.com>
2021-04-04 13:17:44 +0200

    Testek megirása 1.0

--- src/Versenyző.ts
+++ src/Versenyző.ts
@@ -1,49 +1,47 @@
-export default class Versenyző{
+export default class Versenyző {
     // Albert Laszlo;Felnott ferfi;HOLE HUNTERS;0;0;10;10;0;0;0;10
     private _név: string;
     private _kategória: string;
     private _egyesület: string;
     private _pontok: number[] = [];
-    
-    
-    public get név(): string{
+
+    public get név(): string {
         return this._név;
     }
-    public get kategória(): string{
+    public get kategória(): string {
         return this._kategória;
     }
-    public get egyesület(): string{
+    public get egyesület(): string {
         return this._egyesület;
+    }
+    public get pontok(): number[] {
+        return this._pontok;
+    }
 
+    //6.feladat
+    public get osszpontSzam(): number {
+        let osszpont: number = 0;
+        for (let i = 2; i < this._pontok.length; i++) {
+            osszpont += this._pontok[i];
+        }
+        if (this._pontok[0] != 0) {
+            osszpont += 10;
+        }
+        if (this._pontok[1] != 0) {
+            osszpont += 10;
+        }
+        return osszpont;
+    }
 
-    }
-//6.feladat
-public get osszpontSzam(): number {
-    let osszpont: number =0;
-    for (let i = 2; i< this._pontok.length; i++)
-    {
-        osszpont += this._pontok[i]
-    }
-    if (this._pontok[0] != 0)
-    {
-        osszpont += 10;
-    }
-    if (this._pontok[1] != 0)
-    {
-       osszpont += 10    
-    }
-    return osszpont;
-}
-
-    public constructor(sor:string){
+    public constructor(sor: string) {
         const m: string[] = sor.split(";");
 
         this._név = m[0];
         this._kategória = m[1];
         this._egyesület = m[2];
         for (let i = 3; i < m.length; i++) {
-            this._pontok.push(parseInt(m[i]))
+            this._pontok.push(parseInt(m[i]));
         }
-        this._pontok = this._pontok.sort((a,b) => a-b);
+        this._pontok = this._pontok.sort((a, b) => a - b);
     }
 }
